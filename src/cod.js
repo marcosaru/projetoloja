d2e0eeed340f2ffc2fa838a4fef1f0ca7832b6f6
Marcos Gonçalves <marcos.aru@hotmail.com>
2022-08-08 23:53:52 -0300

    upload

---
+++ src/cod.js
@@ -0,0 +1,11 @@
+
+$('#checar').click(function () {
+
+    console.log('ola')
+})
+$('#flexSwitchCheckDefault').click(function () {
+        $('#menubar').toggleClass('bg-dark')
+        $('section').toggleClass('bg-dark')
+        $('footer').toggleClass('bg-dark')
+})
+
